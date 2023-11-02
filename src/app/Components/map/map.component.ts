56d79f7d155e49aedf8ad0d97da2436d6932277d
Samuel-Curtis <samuel.curtis.702@gmail.com>
2023-11-02 12:49:48 -0400

    Added Typescript for mouseover functionality to print the name of each country when they are hovered over

--- src/app/Components/map/map.component.ts
+++ src/app/Components/map/map.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.addEventListeners();
+  }
+
+  addEventListeners(): void {
+    let paths = document.querySelectorAll('path');
+
+    for(let i = 0; i < paths.length; i++) {
+      paths[i].addEventListener('mouseover', () => {
+        let name: string | null = paths[i].getAttribute('name');
+
+        if (name != null) {
+          this.getCountryData(name);
+        }
+      })
+    }
+  }
+
+  getCountryData(name: string) {
+    console.log(`name: ${name}`);
+    
+  }
 }

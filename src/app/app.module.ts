1da910d733f7b1be5a13c17ac27c9ae860a6a5a6
Samuel-Curtis <samuel.curtis.702@gmail.com>
2023-11-02 12:23:34 -0400

    Created main page with map and data-display components. Aligned components on main page. Added world-map svg to assets folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './Components/map/map.component';
+import { DataDisplayComponent } from './Components/data-display/data-display.component';
+import { MainComponent } from './Page/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    DataDisplayComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
